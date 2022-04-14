9636e802a09c32d5da05f92ec5ae04a104512298
Qabas <qabasalani1@gmail.com>
2022-04-14 09:03:38 -0400

    challange 6 fizzbuzz  solution 1 challange

--- index.js
+++ index.js
@@ -159,24 +159,24 @@
 // challenge 6
 ///////////////////////
 
-// Fizz buzz
+// Fizz buzz challenge
 /////////////////
 
 // solution one
 
-function fizzBuzz() {
-  for (let i = 1; i <= 100; i++) {
-    if (i % 15 === 0) {
-      console.log("fizzBuzz");
-    } else if (i % 3 === 0) {
-      console.log("fizz");
-    } else if (i % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
-    }
-  }
-}
+// function fizzBuzz() {
+//   for (let i = 1; i <= 100; i++) {
+//     if (i % 15 === 0) {
+//       console.log("fizzBuzz");
+//     } else if (i % 3 === 0) {
+//       console.log("fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
 
 const output = fizzBuzz();
 console.log(output);
