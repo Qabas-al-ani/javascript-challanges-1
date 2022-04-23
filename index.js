74e54c8b4daee704e93345841b8cdb88efc77764
Qabas <qabasalani1@gmail.com>
2022-04-23 19:06:52 -0400

    challenge sum all primes first solution

--- index.js
+++ index.js
@@ -237,6 +237,23 @@
 
 /////////////////////////////
 
-function sumAllPrimes() {
-  return sumAllPrimes;
+function sumAllPrimes(num) {
+  let total = 0;
+
+  function isPrime(i) {
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      total += i;
+    }
+  }
+  return total;
 }
+console.log(sumAllPrimes(10));
